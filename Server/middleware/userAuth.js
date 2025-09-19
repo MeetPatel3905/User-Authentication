988730167039a63fdc00eb937b5ede01e16273e7
Meet Patel <p.meet9305@gmail.com>
2025-09-20 00:50:29 +0530

    msg

--- Server/middleware/userAuth.js
+++ Server/middleware/userAuth.js
@@ -1,26 +1,25 @@
 import jwt from 'jsonwebtoken';
 
 const userAuth = async (req, res, next) => {
-    const {token} = req.cookies;
+    const { token } = req.cookies;
 
     if (!token) {
         return res.status(401).json({ success: false, message: "Unauthorized: Login again" });
     }
+
     try {
         const tokendecode = jwt.verify(token, process.env.JWT_SECRET);
 
-         if(tokendecode.id){
-             req.body.userId = tokendecode.id
-         } else {
-             return res.status(401).json({ success: false, message: "Unauthorized: Login again" });
-         }
+        if (tokendecode.userId) {
+            req.body.userId = tokendecode.userId;  
+        } else {
+            return res.status(401).json({ success: false, message: "Unauthorized: Login again" });
+        }
 
         next();
-
     } catch (error) {
         return res.status(401).json({ success: false, message: error.message });
     }
-     
-}
+};
 
-export default userAuth; 
+export default userAuth;
